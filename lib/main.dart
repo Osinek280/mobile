cfcaf898b64b1fc5b8d38a74f0e8657641bb9a0d
Szymon <szymon.osinski.2009@gmail.com>
2025-06-25 09:47:42 +0200

    add homePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,134 @@
 import 'package:flutter/material.dart';
 
+const _fontFamily = 'SF Pro Text'; // dodaj do pubspec.yaml i assets/fonts
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFF007AFF),
+    onPrimary: Color(0xFFFFFFFF),
+    secondary: Color(0xFFD7D7D7),
+    onSecondary: Color(0xFF131313),
+    error: Color(0xFFD9534F),
+    onError: Color(0xFFFFFFFF),
+    surface: Color(0xFFF6F6F6),
+    onSurface: Color(0xFF131313), // dawny onBackground
+  ),
+  scaffoldBackgroundColor: Color(0xFFFFFFFF), // Bg
+  dividerColor: Color(0xFFF2F2F2),
+);
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark,
+    primary: Color(0xFF007AFF),
+    onPrimary: Color(0xFFFFFFFF),
+    secondary: Color(0xFF4C4C4C),
+    onSecondary: Color(0xFFFFFFFF),
+    error: Color(0xFFD9534F),
+    onError: Color(0xFFFFFFFF),
+    surface: Color(0xFF00254D),
+    onSurface: Color(0xFFFFFFFF), // dawny onBackground
+  ),
+  scaffoldBackgroundColor: Color(0xFF131313), // Bg
+  dividerColor: Color(0xFF393939),
+);
+
+final TextTheme customTextTheme = TextTheme(
+  // Headliney
+  displayLarge: TextStyle(
+    fontSize: 40,
+    height: 48 / 40,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.bold,
+    fontFamily: _fontFamily,
+  ),
+  displayMedium: TextStyle(
+    fontSize: 32,
+    height: 40 / 32,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  ),
+  displaySmall: TextStyle(
+    fontSize: 24,
+    height: 32 / 24,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  ),
+
+  // Title
+  titleLarge: TextStyle(
+    fontSize: 24,
+    height: 32 / 24,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  ),
+  titleMedium: TextStyle(
+    fontSize: 20,
+    height: 24 / 20,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  ),
+  titleSmall: TextStyle(
+    fontSize: 18,
+    height: 24 / 18,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  ),
+
+  // Label
+  labelLarge: TextStyle(
+    fontSize: 20,
+    height: 24 / 20,
+    letterSpacing: -0.4,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  ),
+  labelMedium: TextStyle(
+    fontSize: 16,
+    height: 20 / 16,
+    letterSpacing: -0.4,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  ),
+  labelSmall: TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    letterSpacing: -0.4,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  ),
+
+  // Body
+  bodyLarge: TextStyle(
+    fontSize: 20,
+    height: 24 / 20,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.normal,
+    fontFamily: _fontFamily,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 16,
+    height: 20 / 16,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.normal,
+    fontFamily: _fontFamily,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    letterSpacing: -0.3,
+    fontWeight: FontWeight.normal,
+    fontFamily: _fontFamily,
+  ),
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,8 +140,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      title: 'Edumate',
+      darkTheme: darkTheme.copyWith(
+        // primarySwatch: Colors.blue,
+        textTheme: customTextTheme,
+        // useMaterial3: true,
+      ),
+      themeMode: ThemeMode.light,
+      theme: lightTheme.copyWith(
+        // primarySwatch: Colors.blue,
+        textTheme: customTextTheme,
+        // useMaterial3: true,
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -28,95 +166,106 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LanguageLearningScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LanguageLearningScreen extends StatelessWidget {
+  const LanguageLearningScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          // padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Speaker.',
+                style: textTheme.displaySmall?.copyWith(
+                  color: Colors.black,
+                ), // 24/32 headlineSmall
+              ),
+              const SizedBox(height: 40),
+              Center(child: Image.asset('assets/home-light2.png')),
+              const SizedBox(height: 20),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Learn english in 30 minute a day',
+                      style: textTheme.displayMedium?.copyWith(
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "Let's start studying!",
+                      style: textTheme.titleSmall?.copyWith(
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 360,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigate to learning screen
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 20,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Start learning',
+                    style: textTheme.labelLarge?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account? ',
+                    style: textTheme.bodySmall?.copyWith(color: Colors.black),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // TODO: dodaj nawigację do ekranu logowania
+                    },
+                    child: Text(
+                      'Log in',
+                      style: textTheme.bodySmall?.copyWith(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
